b9244ed1deb90554c415bfc96ea09fcd6b62708a
Barnett-Wan <peja3020@gmail.com>
2014-07-26 16:30:27 +0800

    new

---
+++ src/back/tag/model/TagVO.java
@@ -0,0 +1,21 @@
+package back.tag.model;
+
+public class TagVO implements java.io.Serializable{
+	private String  tag_no;
+	private String  tag_desc;
+	
+	public String getTag_no() {
+		return tag_no;
+	}
+	public void setTag_no(String tag_no) {
+		this.tag_no = tag_no;
+	}
+	public String getTag_desc() {
+		return tag_desc;
+	}
+	public void setTag_desc(String tag_desc) {
+		this.tag_desc = tag_desc;
+	}
+
+    
+}
